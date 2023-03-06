a84e7b07a449a9b2fde86f7d2b159fdba67ddd9d
Shreya Vinod Barad <shreyavbarad72@gmail.com>
2023-03-06 22:42:51 +0530

    Database for the Online Food Delivery Website!

---
+++ Database for Online Food Delivery Website/Database Version-01/assignment-03and04.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Generation Time: Mar 06, 2023 at 06:05 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `assignment-03and04`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_admin`
+--
+
+CREATE TABLE `table_admin` (
+  `Id` int(10) UNSIGNED NOT NULL,
+  `FullName` varchar(100) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Admin Table';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_category`
+--
+
+CREATE TABLE `table_category` (
+  `Id` int(10) UNSIGNED NOT NULL,
+  `Title` varchar(100) NOT NULL,
+  `ImageName` varchar(255) NOT NULL,
+  `Featured` varchar(10) NOT NULL,
+  `Active` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_food`
+--
+
+CREATE TABLE `table_food` (
+  `Id` int(10) UNSIGNED NOT NULL,
+  `Title` varchar(150) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  `ImageName` varchar(255) NOT NULL,
+  `CategoryId` int(10) UNSIGNED NOT NULL,
+  `Featured` varchar(10) NOT NULL,
+  `Active` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Table Food';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_order`
+--
+
+CREATE TABLE `table_order` (
+  `Id` int(10) UNSIGNED NOT NULL,
+  `Food` varchar(150) NOT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  `Quantity` int(10) NOT NULL,
+  `Total` decimal(10,2) NOT NULL,
+  `OrderDate` datetime NOT NULL,
+  `Status` varchar(50) NOT NULL,
+  `CustomerName` varchar(150) NOT NULL,
+  `CustomerContact` varchar(20) NOT NULL,
+  `CustomerEmail` varchar(150) NOT NULL,
+  `CustomerAddress` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='Table Order';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `table_admin`
+--
+ALTER TABLE `table_admin`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `table_category`
+--
+ALTER TABLE `table_category`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `table_food`
+--
+ALTER TABLE `table_food`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `table_order`
+--
+ALTER TABLE `table_order`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `table_admin`
+--
+ALTER TABLE `table_admin`
+  MODIFY `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `table_category`
+--
+ALTER TABLE `table_category`
+  MODIFY `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `table_food`
+--
+ALTER TABLE `table_food`
+  MODIFY `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `table_order`
+--
+ALTER TABLE `table_order`
+  MODIFY `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
