f98b40e7d08197c62d62f8680ef634a6f9c958e3
Shreya Vinod Barad <shreyavbarad72@gmail.com>
2023-03-07 18:47:58 +0530

    Work on  Admin Panel has Started!

---
+++ BackEnd/AdminPanel/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Food Delivery Website-Admin Panel</title>
+    <link rel="stylesheet" href="AdminPanel.css">
+</head>
+<body>
+    <!-- Menu Section Starts Here -->
+    <div class="Menu">
+        <div class="Wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Menu Section Ends Here -->
+
+    <!-- Main Content Section Starts Here -->
+    <div class="MainContent">
+        <div class="Wrapper">
+            <Strong>DashBoard</Strong>
+        </div>
+    </div>
+    <!-- Main Content Section Ends Here -->
+
+    <!-- Footer Section Starts Here -->
+    <div class="Footer">
+        <div class="Wrapper">
+            <p class="AlignCenter">
+                All Rights Reserved. Designed by <a href="#">Shreya Barad</a>.
+            </p>
+        </div>
+    </div>
+    <!-- Footer Section Ends Here -->
+
+</body>
+</html>
